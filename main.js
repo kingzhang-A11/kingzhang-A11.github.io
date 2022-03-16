a4ec130754392e087165715a86870aa3827984c1
张文美 <8486998+zhang-wenmei@user.noreply.gitee.com>
2022-03-16 11:42:11 +0800

    2

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-window.onload = function() {
+window.onload = function () {
     AFRAME.registerComponent('videohandler', {
-        init: function () {
-            let marker = this.el;
+      init: function () {
+        var marker = this.el;
 
-            this.vid = document.querySelector("#vid");
+        this.vid = document.querySelector("#vid");
 
-            marker.addEventListener('markerFound', function () {
-                this.vid.play();
-            }.bind(this));
+        marker.addEventListener('markerFound', function () {
+          // 识别到标识图，开始播放视频
+          this.vid.play();
+        }.bind(this));
 
-    marker.addEventListener('markerLost', function() {
-        this.vid.pause();
-        this.vid.currentTime = 0;
-    }.bind(this));
-        }
+        marker.addEventListener('markerLost', function () {
+          // 丢失标识图，停止播放视频
+          this.vid.pause();
+          this.vid.currentTime = 0;
+        }.bind(this));
+      }
     });
-};
+  };
